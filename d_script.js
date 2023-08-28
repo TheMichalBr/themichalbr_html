9bad59e9c8ae94fa114dd72d741b1892e154b788
TheMichalBr <br.misak@gmail.com>
2023-08-28 13:24:55 +0200

    Update

--- d_script.js
+++ d_script.js
@@ -1,1 +1,20 @@
 alert("The web is currently under maintenance!");
+
+
+const container = document.querySelector(".container");
+const primaryNav = document.querySelector(".nav__list");
+const toggleButton = document.querySelector(".nav-toggle");
+
+toggleButton.addEventListener("click", () => {
+    const isExpanded = primaryNav.getAttribute("aria-expanded");
+    primaryNav.setAttribute(
+        "aria-expanded",
+        isExpanded === "false" ? "true" : "false"
+    );
+});
+
+container.addEventListener("click", (e) => {
+    if (!primaryNav.contains(e.target) && !toggleButton.contains(e.target)) {
+        primaryNav.setAttribute("aria-expanded", "false");
+    }
+});
